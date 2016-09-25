1d8c7472075c61680b6890f46e39751fc0cd5537
John Liu <Liudesign@gmail.com>
2016-09-25 18:57:03 -0400

    Added exercises to GH from AJAX videos

--- intranet/js/widget.js
+++ intranet/js/widget.js
@@ -1,20 +1,29 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '../data/employees.json');
-xhr.onreadystatechange = function () {
-  if(xhr.readyState === 4 && xhr.status === 200) {
-    var employees = JSON.parse(xhr.responseText);
+$(document).ready(function () {
+  $.getJSON('../data/employees.json', function (data) {
     var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i += 1) {
-      if (employees[i].inoffice === true) {
-        statusHTML += '<li class="in">';
+    $.each(data,function (index, employee) {
+      if (employee.inoffice === true) {
+        statusHTML +='<li class="in">';
       } else {
-        statusHTML += '<li class="out">';
+        statusHTML +='<li class="out">';
       }
-      statusHTML += employees[i].name;
-      statusHTML += '</li>';
-    }
+      statusHTML += employee.name + '</li>';
+    });
     statusHTML += '</ul>';
-    document.getElementById('employeeList').innerHTML = statusHTML;
-  }
-};
-xhr.send();
+    $('#employeeList').html(statusHTML)
+  }); // end getJSON
+  
+  $.getJSON('../data/rooms.json', function (data) {
+    var statusHTML = '<ul class="rooms">';
+    $.each(data,function (index, room) {
+      if (room.available === true) {
+        statusHTML +='<li class="empty">';
+      } else {
+        statusHTML +='<li class="full">';
+      }
+      statusHTML += room.room + '</li>';
+    });
+    statusHTML += '</ul>';
+    $('#roomList').html(statusHTML)
+  }); // end getJSON
+}); // end ready
